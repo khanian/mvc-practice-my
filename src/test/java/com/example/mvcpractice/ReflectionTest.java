4e1f75fc0b20aeafa75dfd3b8fbea404519c084a
khanian <khanylee@gmail.com>
2023-07-11 21:05:47 +0900

    second commit

--- src/test/java/com/example/mvcpractice/ReflectionTest.java
+++ src/test/java/com/example/mvcpractice/ReflectionTest.java
@@ -2,6 +2,7 @@ package com.example.mvcpractice;
 
 import com.example.annotation.Controller;
 import com.example.annotation.Service;
+import com.example.model.User;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
@@ -9,9 +10,13 @@ import org.slf4j.LoggerFactory;
 
 import java.beans.ConstructorProperties;
 import java.lang.annotation.Annotation;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @Controller 애노테이션 설정돼 있는 모든 클래스를 찾아서 출력한다.
@@ -23,6 +28,37 @@ public class ReflectionTest {
         Set<Class<?>> beans = getTypesAnnotatedWith(List.of(Controller.class, Service.class));
 
         logger.info("beans: [{}]", beans);
+    }
+
+    @Test
+    void showClass() {
+        Class<User> clazz = User.class;
+        logger.info(clazz.getName());
+
+        logger.info("User all declared fields: [{}]", Arrays.stream(clazz.getDeclaredFields()).collect(Collectors.toList()));
+        logger.info("User all declared constructors: [{}]", Arrays.stream(clazz.getDeclaredConstructors()).collect(Collectors.toList()));
+        logger.info("User all declared methods: [{}]", Arrays.stream(clazz.getDeclaredMethods()).collect(Collectors.toList()));
+    }
+
+    @Test
+    void load() throws ClassNotFoundException {
+        // 1
+        Class<User> clazz = User.class;
+
+        // 2
+        User user = new User("khanian", "홍길동");
+        Class<? extends User> clazz2 = user.getClass();
+
+        // 3
+        Class<?> clazz3 = Class.forName("com.example.model.User");
+
+        logger.info("clazz: [{}}", clazz);
+        logger.info("clazz2: [{}}", clazz2);
+        logger.info("clazz3: [{}}", clazz3);
+
+        assertThat(clazz == clazz2).isTrue();
+        assertThat(clazz2 == clazz3).isTrue();
+        assertThat(clazz3 == clazz).isTrue();
     }
 
     private static Set<Class<?>> getTypesAnnotatedWith(List<Class<? extends Annotation>> annotations) {
